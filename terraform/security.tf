eb28ea48065f88d8478099a5dd1a2006a4f8f09b
TRONStack Developer <tronstack@example.com>
2025-04-18 00:44:31 +0100

    Update security configuration in Terraform

--- terraform/security.tf
+++ terraform/security.tf
@@ -16,3 +16,26 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "ecs_sg" {
+  name   = "ecs-instance-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # You can limit this to trusted IPs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ECS Instance SG"
+  }
+}
