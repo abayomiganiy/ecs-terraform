2103f83725ea6819e5fb41cf7743672e8c742e6e
TRONStack Developer <tronstack@example.com>
2025-04-18 01:02:43 +0100

    add ami

---
+++ terraform/ami.tf
@@ -0,0 +1,15 @@
+data "aws_ami" "ecs_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  owners = ["amazon"]
+}
