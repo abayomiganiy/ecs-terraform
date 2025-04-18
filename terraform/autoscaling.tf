66a5163f7aa385844ac972729bc18354d6840b1c
TRONStack Developer <tronstack@example.com>
2025-04-18 01:42:49 +0100

    Update autoscaling group to use specific public subnets instead of data source

--- terraform/autoscaling.tf
+++ terraform/autoscaling.tf
@@ -1,11 +1,14 @@
 resource "aws_autoscaling_group" "ecs_asg" {
-  name_prefix          = "ecs-asg-"
-  max_size             = 4
-  min_size             = 1
-  desired_capacity     = 2
-  vpc_zone_identifier  = data.aws_subnets.public.ids
-  health_check_type    = "EC2"
-  target_group_arns    = [aws_lb_target_group.app_tg.arn]
+  name_prefix      = "ecs-asg-"
+  max_size         = 4
+  min_size         = 1
+  desired_capacity = 2
+  vpc_zone_identifier = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+  health_check_type = "EC2"
+  target_group_arns = [aws_lb_target_group.app_tg.arn]
   launch_template {
     id      = aws_launch_template.ecs_template.id
     version = "$Latest"
